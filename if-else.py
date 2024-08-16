b7f835760364c8ea03c1408d73cb9758d898ade1
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-08-16 16:56:16 +0530

    If-else

---
+++ if-else.py
@@ -0,0 +1,16 @@
+#here we will gonna learn about conditions
+
+"""Syntax :
+
+if Condition:
+    code
+    code
+"""
+
+a = int(input())
+if a > 5:
+    print("%d is greater than 5" %a)
+elif a < 5:
+    print("%d is smaller than 5" %a)
+else:
+    pass        
