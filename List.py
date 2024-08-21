1a2158acc5d864a3e8449f38327fb68ce03169bd
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-08-21 16:36:47 +0530

    updated

---
+++ List.py
@@ -0,0 +1,12 @@
+#here we'll learn about lists in python
+
+l1 = [10,20,30,40,50]
+l2 = [80,50,40,90,70]
+
+l1.append(60)
+l2.append(100)
+
+print(l1[5])
+print(l1)
+
+#behaves most of the same as array but not completely
---
+++ Range.py
@@ -0,0 +1,7 @@
+#creating an object for the range function
+
+r1 = range(1,10,2)
+
+#printing the elements of the range 
+for var in r1:
+    print(var)
